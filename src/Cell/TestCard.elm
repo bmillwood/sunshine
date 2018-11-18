5146d5cac4d968af7d9048feda08806503a688b1
Ben Millwood <thebenmachine+git@gmail.com>
2018-11-18 23:23:30 +0800

    replace PRNG with something more meaningful

--- src/Cell/TestCard.elm
+++ src/Cell/TestCard.elm
@@ -17,27 +17,7 @@ boost { timeStep } cell = cell
 
 value : Cell -> Float
 value (C (cx, cy)) =
-  {- I can't remember any PRNGs but I remember that inverting discrete log
-     is hard. -}
-  let
-      m = 107
-      loop x y acc =
-        if y == 0
-        then acc
-        else
-          loop
-            (modBy m (x * x))
-            (y // 2)
-            (if modBy 2 y == 1 then modBy m (acc * x) else acc)
-      positivize n =
-        if n >= 0
-        then 2 * n
-        else 2 * abs n - 1
-      notDiv d n =
-        n + n // (d - 1) + 1
-      tweak n = notDiv m (positivize n + 7)
-  in
-  toFloat (loop (tweak cx) (tweak cy) 1) / toFloat m
+  sin (toFloat (cx + cy) / 4) * -0.3 + 0.5
 
 step : { timeStep : Timespan } -> (Pt -> Maybe Cell) -> Cell -> (Cell, Cmd Msg)
 step { timeStep } getNeighbour cell = (cell, Cmd.none)
